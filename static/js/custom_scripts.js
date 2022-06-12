8e2609e99249d2cf9e45c10ef050523f2bf9d659
walkersorlie <walkersorlie@gmail.com>
2022-06-12 11:07:05 -0700

    Initial commit. All site basics working except maybe contact form. No admin set up yet

---
+++ static/js/custom_scripts.js
@@ -0,0 +1,12 @@
+
+function handleSubmit() {
+  let myForm = document.getElementById("contactForm");
+  let formData = new FormData(myForm);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
