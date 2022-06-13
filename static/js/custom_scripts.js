cb0fafa994471f60f8c705ff983a68263e92f10c
walkersorlie <walkersorlie@gmail.com>
2022-06-13 15:26:51 -0700

    Adding Bootstrap to make form

--- static/js/custom_scripts.js
+++ static/js/custom_scripts.js
@@ -2,12 +2,14 @@
 function handleSubmit() {
   let myForm = document.getElementById("contactForm");
   let formData = new FormData(myForm);
-  console.log(formData.toString());
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => console.log("Form successfully submitted"))
+    .then(() => {
+		
+		console.log("Form successfully submitted")
+	})
     .catch((error) => alert(error));
 };
