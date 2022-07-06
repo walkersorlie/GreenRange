792ada035a0d35fae7b7ef47d588f01f75d022ab
Walker Sorlie <walkersorlie@gmail.com>
2022-07-06 13:03:44 -0300

    Refactor form

--- static/js/custom_scripts.js
+++ static/js/custom_scripts.js
@@ -1,15 +1,29 @@
+const contactForm = document.getElementById('contactForm');
+const submitButton = document.getElementById('submitButton');
 
-function handleSubmit() {
-  let myForm = document.getElementById("contactForm");
-  let formData = new FormData(myForm);
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => {
-		document.getElementById("formSubmitSuccess").style.display = 'block';
-		console.log("Form successfully submitted");
-	})
-    .catch((error) => alert(error));
+
+function handleSubmit(event) {
+	if (!contactForm.checkValidity()) {
+		event.preventDefault();
+		event.stopPropagation();
+	} else {
+		let formData = new FormData(contactForm);
+		fetch("/", {
+		method: "POST",
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		body: new URLSearchParams(formData).toString(),
+		})
+		.then(() => {
+			document.getElementById("formSubmitSuccess").style.display = 'block';
+			console.log("Form successfully submitted");
+		})
+		.catch((error) => {
+			alert('There was a problem submitting the form. Please try again.');
+			console.log(error);
+		});
+	}
+	
+	contactForm.classList.add('was-validated');
 };
+
+submitButton.addEventListener('click', handleSubmit, false);
